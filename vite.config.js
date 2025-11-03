e5748f9abca982c27bef64f7860503c3a7d418be
Joanna <j.puno0909@gmail.com>
2025-11-02 19:16:10 -0700

    adds vitest and rtl setup

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vitest/config"
 import react from '@vitejs/plugin-react';
 import path from 'path'
 
@@ -19,5 +19,11 @@ export default defineConfig({
     strictPort: true,
     host: true,
     origin: "http://0.0.0.0:8080",
+  },
+  test: {
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+    css: true,
+    globals: true,
   },
 })
